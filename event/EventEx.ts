69e53194ca0fb35878ea11827dad3c2bde847fbb
ttian99 <176553611@qq.com>
2018-07-13 17:51:34 +0800

    修复EventEx不能继承的bug

--- event/EventEx.ts
+++ event/EventEx.ts
@@ -1,5 +1,5 @@
 
-class EventEx {
+export default class EventEx {
   /**
    * @param {cc.Node} node // 这个node节点是你要监听点击的节点
    * @param {cc.Node} target //这个 target 节点是你的事件处理代码组件所属的节点
@@ -7,7 +7,7 @@ class EventEx {
    * @param {String} handler 回调函数的名字
    * @param {*} customEventData 用户传递的事件数据
    */
-  public static addClickEvent(node: cc.Node, target: cc.Node, component: string, handler: string, customEventData: any) {
+  public addClickEvent(node: cc.Node, target: cc.Node, component: string, handler: string, customEventData: any) {
     const clickEventHandler: cc.Component.EventHandler = new cc.Component.EventHandler();
     clickEventHandler.target = target;
     clickEventHandler.component = component;
@@ -20,14 +20,14 @@ class EventEx {
 
   /**
    * @method addSlideEvent
-   * 
+   *
    * @param {cc.Node} node // 这个node节点是你要监听滑动事件的节点
    * @param {cc.Node} target //这个 target 节点是你的事件处理代码组件所属的节点
    * @param {String} component 组件脚本的文件名
    * @param {String} handler 回调函数的名字
    * @param {*} customEventData 用户传递的事件数据
    */
-  public static addSlideEvent(node: cc.Node, target: cc.Node, component: string, handler: string, customEventData: any) {
+  public addSlideEvent(node: cc.Node, target: cc.Node, component: string, handler: string, customEventData: any) {
     const eventHandler: cc.Component.EventHandler = new cc.Component.EventHandler();
     eventHandler.target = target;
     eventHandler.component = component;
@@ -40,14 +40,14 @@ class EventEx {
 
   /**
   * @method addScrollView 添加滑动事件
-  * 
+  *
   * @param {cc.Node} node // 这个node节点是你要监听滑动事件的节点
   * @param {cc.Node} target //这个 target 节点是你的事件处理代码组件所属的节点
   * @param {String} component 组件脚本的文件名
   * @param {String} handler 回调函数的名字
   * @param {*} customEventData 用户传递的事件数据
   */
-  public static addScrollView(node: cc.Node, target: cc.Node, component: string, handler: string, customEventData: any) {
+  public addScrollView(node: cc.Node, target: cc.Node, component: string, handler: string, customEventData: any) {
     const scrollViewEventHandler: cc.Component.EventHandler = new cc.Component.EventHandler();
     scrollViewEventHandler.target = target; // 这个 node 节点是你的事件处理代码组件所属的节点
     scrollViewEventHandler.component = component;// 这个是代码文件名
@@ -65,11 +65,9 @@ class EventEx {
    * @param {String} eventName // 事件名称
    * @param {*} 用户传递的事件数据
    */
-  public static dispatchEvent(node: cc.Node, eventName: string, customEventData: any) {
+  public dispatchEvent(node: cc.Node, eventName: string, customEventData: any) {
     const event: cc.Event.EventCustom = new cc.Event.EventCustom(eventName, true);
     event.setUserData(customEventData);
     cc.isValid(node) && node.dispatchEvent(event);
   }
 }
-
-export default EventEx;
